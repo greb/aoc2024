c9e92ef97988b2a0a88fcfc3fcd79d1f85fc8ee8
greb <greb>
2024-12-20 14:44:58 +0100

    Solve day 20

--- days/day20.py
+++ days/day20.py
@@ -1,48 +1,53 @@
 import collections
+import itertools
 
 def parse(inp):
     walls = set()
-
     for y, row in enumerate(inp.splitlines()):
         for x,c in enumerate(row):
             match c:
                 case 'S':
                     start = x,y
-                case 'E':
-                    end = x,y
                 case '#':
                     walls.add((x,y))
-    return walls, start, end
+
+    dist = {start: 0}
+    queue = collections.deque([start])
+    while queue:
+        curr = queue.popleft()
+        for nxt in neighbors(curr):
+            if nxt in walls or nxt in dist:
+                continue
+            dist[nxt] = dist[curr] + 1
+            queue.append(nxt)
+
+    return dist
 
 DIRS = [(1,0),(0,1),(-1,0),(0,-1)]
 def neighbors(pos):
     for dir in DIRS:
         yield tuple(p+d for p,d in zip(pos, dir))
 
-def part1(inp):
-    walls, start, end = inp
 
-    shortest = dict()
-    queue = collections.deque([(0,start)])
-    while queue:
-        cnt, curr = queue.popleft()
-        for nxt in neighbors(curr):
-            if nxt in shortest:
-                continue
-            shortest[nxt] = cnt+1, curr
-            if nxt not in walls:
-                queue.append((cnt+1, nxt))
+def manhatten(a,b):
+    return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
 
-    counter = collections.Counter()
-    for curr, (cnt, prev) in shortest.items():
-        if curr not in walls:
-            continue
-        for nxt in neighbors(curr):
-            if nxt == prev or nxt in walls or nxt not in shortest:
-                continue
-            cheat_cnt = shortest[nxt][0] - cnt - 1
-            counter[cheat_cnt] += 1
+def part1(dist):
+    cnt = 0
+    for (a,da),(b,db) in itertools.combinations(dist.items(), 2):
+        d = manhatten(a,b)
+        if d == 2:
+            if db-da-d >= 100:
+                cnt += 1
+    return cnt
 
-    return sum(n for c,n in counter.items() if c >= 100)
 
+def part2(dist):
+    cnt = 0
+    for (a,da),(b,db) in itertools.combinations(dist.items(), 2):
+        d = manhatten(a,b)
+        if d <= 20:
+            if db-da-d >= 100:
+                cnt += 1
+    return cnt
